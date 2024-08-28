007b27558a0564155deda60664142b2f5fc0087e
wendwin <wendwin12@gmail.com>
2024-08-28 11:32:47 +0700

    create login and logout authentication features

--- posproject/posapp/urls.py
+++ posproject/posapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from posapp import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('login/', LoginView.as_view(), name='login'),  
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]

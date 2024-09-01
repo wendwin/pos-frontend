918b8521f4357e4913e29fe50b45cdb10e807be0
wendwin <wendwin12@gmail.com>
2024-09-01 22:18:28 +0700

    Add a feature to add items to the cart, including options to decrease quantity and remove items

--- posproject/posapp/static/js/scripts.js
+++ posproject/posapp/static/js/scripts.js
@@ -17,7 +17,6 @@ document.addEventListener("alpine:init", () => {
         .then((resp) => resp.json())
         .then((data) => {
           this.itemsProduk = data;
-          console.log(data);
         })
         .catch((e) => {
           console.log(e);
@@ -29,11 +28,115 @@ document.addEventListener("alpine:init", () => {
         .then((resp) => resp.json())
         .then((data) => {
           this.itemsProduk = data;
-          console.log(data);
         })
         .catch((e) => {
           console.log(e);
         });
     },
   }));
+
+  Alpine.store("cartData", {
+    items: [],
+
+    // total keseluran
+    total: 0,
+    quantity: 0,
+
+    getListItem() {
+      let getCart = JSON.parse(localStorage.getItem("cart"));
+      this.items = getCart;
+    },
+
+    add(newItem) {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+      let existingProduct = cart.find(
+        (item) => item.id_item === newItem.id_item
+      );
+
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+        existingProduct.total += parseInt(existingProduct.harga);
+        this.quantity++;
+        this.total += parseInt(newItem.harga);
+      } else {
+        cart.push({
+          id_item: newItem.id_item,
+          nama_item: newItem.nama_item,
+          nama_kategori: newItem.nama_kategori,
+          harga: newItem.harga,
+          gambar: newItem.gambar,
+          quantity: 1,
+          total: parseInt(newItem.harga),
+        });
+        this.quantity++;
+        this.total += parseInt(newItem.harga);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      let getCart = JSON.parse(localStorage.getItem("cart"));
+      this.items = getCart;
+    },
+
+    kurang(itemKurang) {
+      let cart = JSON.parse(localStorage.getItem("cart"));
+
+      let existingProduct = cart.find(
+        (item) => item.id_item === itemKurang.id_item
+      );
+
+      if (existingProduct) {
+        if (existingProduct.quantity > 1) {
+          existingProduct.quantity -= 1;
+          existingProduct.total -= parseInt(itemKurang.harga);
+          this.quantity--;
+          this.total -= parseInt(itemKurang.harga);
+        }
+      }
+      localStorage.setItem("cart", JSON.stringify(cart));
+      this.getListItem();
+    },
+
+    delete(itemDelete) {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      // let existingProduct = cart.filter(
+      //   (item) => item.id_item !== itemDelete.id_item
+      // );
+      let existingProductIndex = cart.findIndex(
+        (item) => item.id_item === itemDelete.id_item
+      );
+
+      if (existingProductIndex !== -1) {
+        let existingProduct = cart[existingProductIndex];
+
+        if (cart.length > 1) {
+          if (existingProduct.quantity > 1) {
+            this.total -= existingProduct.total;
+            existingProduct.quantity -= 1;
+            existingProduct.total =
+              existingProduct.quantity * parseInt(itemDelete.harga);
+            cart.splice(existingProductIndex, 1);
+          } else {
+            this.total -= parseInt(itemDelete.harga);
+            cart.splice(existingProductIndex, 1);
+          }
+        } else {
+          if (existingProduct.quantity > 1) {
+            this.total -= existingProduct.total;
+            existingProduct.quantity -= 1;
+            existingProduct.total =
+              existingProduct.quantity * parseInt(itemDelete.harga);
+            cart.splice(existingProductIndex, 1);
+          } else {
+            this.total -= parseInt(itemDelete.harga);
+
+            cart.splice(existingProductIndex, 1);
+          }
+        }
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      this.getListItem();
+    },
+  });
 });
